56a26050f57cb0dbd9e9ad3f4f5202d29f91250f
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-12-05 01:06:55 -0300

    Refactoring the Performance Enum to use Strategy Pattern

--- src/main/java/br/com/lucasbertoloto/service/Performance.java
+++ src/main/java/br/com/lucasbertoloto/service/Performance.java
@@ -1,5 +1,26 @@
 package br.com.lucasbertoloto.service;
 
+import java.math.BigDecimal;
+
 public enum Performance {
-    GOOD, GREAT, TO_DESIRED
+    GOOD{
+        @Override
+        public BigDecimal getPercentageReadjustment() {
+            return new BigDecimal("0.15");
+        }
+    },
+    GREAT {
+        @Override
+        public BigDecimal getPercentageReadjustment() {
+            return new BigDecimal("0.20");
+        }
+    },
+    TO_DESIRED {
+        @Override
+        public BigDecimal getPercentageReadjustment() {
+            return new BigDecimal("0.03");
+        }
+    };
+
+    public abstract BigDecimal getPercentageReadjustment();
 }
--- src/main/java/br/com/lucasbertoloto/service/ReadjustmentService.java
+++ src/main/java/br/com/lucasbertoloto/service/ReadjustmentService.java
@@ -7,18 +7,9 @@ import java.math.BigDecimal;
 public class ReadjustmentService {
 
     public void grantReadjustment(Funcionario funcionario, Performance performance) {
-
-        if(performance == Performance.TO_DESIRED) {
-            BigDecimal readjustment = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-            funcionario.readjustmentSalary(readjustment);
-        } else if (performance == Performance.GOOD) {
-            BigDecimal readjustment = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-            funcionario.readjustmentSalary(readjustment);
-        } else if (performance == Performance.GREAT) {
-            BigDecimal readjustment = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-            funcionario.readjustmentSalary(readjustment);
-        }
-
+        BigDecimal percentageReadjustment = performance.getPercentageReadjustment();
+        BigDecimal readjustment = funcionario.getSalario().multiply(percentageReadjustment);
+        funcionario.readjustmentSalary(readjustment);
     }
 
 }
