04e3452f78e58e6fedbdf6290f6435a8b1f65f60
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-12-05 01:38:42 -0300

    Refactoring using annotation BeforeEach

--- src/test/java/br/com/lucasbertoloto/service/ReadjustmentServiceTest.java
+++ src/test/java/br/com/lucasbertoloto/service/ReadjustmentServiceTest.java
@@ -2,6 +2,7 @@ package br.com.lucasbertoloto.service;
 
 import br.com.lucasbertoloto.modelo.Funcionario;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -9,26 +10,29 @@ import java.time.LocalDate;
 
 public class ReadjustmentServiceTest {
 
+    private ReadjustmentService readjustmentService;
+    private Funcionario funcionario;
+
+    @BeforeEach
+    private void initialize() {
+        readjustmentService = new ReadjustmentService();
+        funcionario = new Funcionario("Fábio", LocalDate.now(), new BigDecimal("1000.00"));
+    }
+
     @Test
     void ShouldReturnThreePorcentInPerformanceToDesired(){
-        ReadjustmentService readjustmentService =  new ReadjustmentService();
-        Funcionario funcionario = new Funcionario("Fábio", LocalDate.now(), new BigDecimal("1000.00"));
         readjustmentService.grantReadjustment(funcionario, Performance.TO_DESIRED);
         Assertions.assertEquals(new BigDecimal("1030.00"),funcionario.getSalario());
     }
 
     @Test
     void ShouldReturnFifteenPorcentInPerformanceGood(){
-        ReadjustmentService readjustmentService =  new ReadjustmentService();
-        Funcionario funcionario = new Funcionario("Fábio", LocalDate.now(), new BigDecimal("1000.00"));
         readjustmentService.grantReadjustment(funcionario, Performance.GOOD);
         Assertions.assertEquals(new BigDecimal("1150.00"),funcionario.getSalario());
     }
 
     @Test
     void ShouldReturnTwentyPorcentInPerformanceGreat(){
-        ReadjustmentService readjustmentService =  new ReadjustmentService();
-        Funcionario funcionario = new Funcionario("Fábio", LocalDate.now(), new BigDecimal("1000.00"));
         readjustmentService.grantReadjustment(funcionario, Performance.GREAT);
         Assertions.assertEquals(new BigDecimal("1200.00"),funcionario.getSalario());
     }
